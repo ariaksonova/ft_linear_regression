91c775469620c50a3bcee05ff1856a3711ea30ac
ariaksonova <ariaksenova@gmail.com>
2019-01-12 13:46:10 +0200

    new_version

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import matplotlib
 matplotlib.use('Agg')
+import numpy
 import matplotlib.pyplot as plt
 import pandas
 import os
 
-import train_model
+from train_model import *
 
 def save(name='', fmt='png'):
     pwd = os.getcwd()
@@ -20,12 +21,15 @@ def get_data():
     predict_data = pandas.read_csv('data.csv', sep=',')
     return(predict_data)
 
-def print_plot(predict_data):
-    plt.plot(predict_data.iloc[:, 0], predict_data.iloc[:, 1], 'ro')
-    save('data_plot', fmt='png')
-    plt.show()
+def print_plot(wt_plot, predict_data, name):
+    fig, ax = plt.subplots()
+    x = numpy.linspace(100)
+    y1 = wt_plot[0] *3650 + wt_plot[1] * 3650 * x
+    ax.plot(x, y1, color="blue")
+    ax.plot(predict_data.iloc[:, 0], predict_data.iloc[:, 1], 'ro')#, wt_plot.iloc[:, 0], wt_plot.iloc[:, 1], 'go')
+    save(name, fmt='png')
 
-if __name__ == "__main__":
-    predict_data = get_data()
-#    print_plot(predict_data)
-    print(functional(0, 0, predict_data))
+predict_data = get_data()
+wt = train_model(predict_data)
+print(wt[0], wt[1])
+print_plot(wt, predict_data, 'data_plot')
--- train_model.py
+++ train_model.py
@@ -1,12 +1,66 @@
-def functional(wt0, wt1, data):
+import pandas
+import numpy
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pandas
+import os
+
+from train_model import *
+
+def save(name='', fmt='png'):
+    pwd = os.getcwd()
+    iPath = './{}'.format(fmt)
+    if not os.path.exists(iPath):
+        os.mkdir(iPath)
+    os.chdir(iPath)
+    plt.savefig('{}.{}'.format(name, fmt), fmt='png')
+    os.chdir(pwd)
+
+def print_plot(predict_data, name):
+    plt.plot(predict_data.iloc[:, 0], predict_data.iloc[:, 1], 'ro')
+    save(name, fmt='png')
+    plt.show()
+
+def functional(wt0, wt1, predict_data):
+    #датасет может быть пустой
     sum = 0
     errors = 0
-    length = len(data)
+    length = len(predict_data)
     for i in range(length):
-        sum += wt1 * data[0][i] + wt0 - data[1][i]
-    errors = sum / length
+        result = wt1 * predict_data.iloc[i][0]
+        result += wt0
+        result -= predict_data.iloc[i][1]
+        result = result ** 2
+        sum += result
+    errors = int(sum / length)
     return errors
 
+def change_wt(wt0, wt1, iter, data):
+    length = len(data)
+    sum0 = 0
+    sum1 = 0
+    iter = 1 / (iter * iter)
+    for i in range(length):
+        sum0 += wt1 * data.iloc[i][0] + wt0 - data.iloc[i][1]
+        sum1 += (wt1 * data.iloc[i][0] + wt0 - data.iloc[i][1]) * data.iloc[i][0]
+    sum0 /= length
+    sum1 /= length
+    wt0 = int(wt0 - iter * sum0)
+    wt1 = int(wt1 - iter * sum1)
+    wt = numpy.array([wt0, wt1])
+    return wt
 
 def train_model(predict_data):
-    pass
+    predict_data = predict_data / 3650
+    wt = numpy.array([0, 0])
+    wt_plot = pandas.DataFrame()
+    wt_plot = wt_plot.append({'errors': functional(0, 0, predict_data), 'iter': 0}, ignore_index=True)
+    for i in range(1, 200):
+        wt = change_wt(wt[0], wt[1], i, predict_data)
+        if functional(wt[0], wt[1], predict_data) == 3:
+            break
+        #wt_plot = wt_plot.append({'errors': functional(wt[0], wt[1], predict_data), 'iter': i}, ignore_index=True)
+    #print_plot(wt_plot, 'wt_plot')
+    return wt
